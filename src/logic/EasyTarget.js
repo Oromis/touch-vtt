a86578c9a3643576b82eccf9369e18cc6501d321
David Bauske <david.bauske@voltstorage.com>
2023-06-18 13:57:21 +0200

    Updated to Foundry version 11

--- src/logic/EasyTarget.js
+++ src/logic/EasyTarget.js
@@ -3,6 +3,12 @@ import {MODULE_NAME} from '../config/ModuleConstants'
 import {EASY_TARGET_OFF, EASY_TARGET_SETTING, EASY_TARGET_SINGLE} from '../config/TouchSettings'
 
 export function initEasyTarget() {
+  // The _handleClickLeft is now private in FoundryVTT v11.
+  // This condition disables easy targeting for now until I find a solution how to override it.
+  if (typeof MouseInteractionManager.prototype._handleClickLeft !== 'function') {
+    return
+  }
+
   wrapMethod('MouseInteractionManager.prototype._handleClickLeft', function (originalMethod, event, ...args) {
     const token = event.currentTarget
     if (isEasyTargetEnabled() && isSelectToolActive() && token instanceof Token && isUnownedToken(this, event)) {
