27eb37637f24324b5263599eb1afe8874e8301a1
Aioros <aioros83@gmail.com>
2024-07-18 16:21:21 -0400

    Fix for annoying double click issue on window headers

--- src/logic/WindowAppAdapter.js
+++ src/logic/WindowAppAdapter.js
@@ -8,7 +8,7 @@ import '../utils/DragDropTouch.js'
 const STYLE_ID = `${MODULE_NAME}-draggable_apps_styles`
 
 const draggableAppStyle = `
-.app, .app .window-title {
+.app, .app .window-header, .app .window-title {
   touch-action: none;
 }
 `
@@ -25,8 +25,28 @@ class WindowAppAdapter {
   constructor() {
     this.lastClickInfo = {target: null, time: 0}
 
-    // Artificial double clicks
+    /*** Double-click management - Start ***/
+    // In both v11 and v12 (but in an especially weird way in v11) double clicks on app windows are triggered inconsistently for touch events
+    // In v12, touching a window header triggers a dblclick
+    // In v11, when rendered, double touching the header doesn't triggere a dblclick (I assume it's some interaction with the draggable),
+    // but after double touching a different section of the window, the behavior becomes the same as v12
+    // The brutal approach here is to just hijack and cancel any dblclick event on an app, and create our own as best as we can
+
+    // Cancel any native dblclick event on apps
+    document.body.addEventListener("dblclick", (evt) => {
+      const isInApp = !!evt.target.closest(".app")
+      if (evt.isTrusted && isInApp) {
+        evt.preventDefault()
+        evt.stopImmediatePropagation()
+        evt.stopPropagation()
+        return false
+      }
+    }, true)
+
+    // Manage click events and decide if we trigger an artificial double click
     $(document.body).on("click", ".app", this.manageTouchDblClick.bind(this))
+
+    /*** Double-click management - End ***/
     
     // Avoid error on Drag and Drop polyfill
     wrapMethod('DragDrop.prototype._handleDragStart', function(originalMethod, event) {
